85a07a3076154f728197db17964dfb8dcdffc1bf
Skaro7 <marc_tau72@hotmail.com>
2026-04-27 11:03:06 +0200

    feat: add ProductRepository interface for Product entity data access

---
+++ src/main/java/com/sonsoflilith/backend/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.sonsoflilith.backend.repository;
+
+import com.sonsoflilith.backend.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryId(Long categoryId);
+}
